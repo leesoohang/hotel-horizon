5b271f8a6d79d5b8ceb91308361129f0bb376dea
Michal_T <michal.trostowiecki@gmail.com>
2024-01-20 18:02:45 +0000

    Add function processHotels to process data for the hotels

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,3 @@
-
 // Fetch cordinates based on user Input
 function getCordinates(location, callback) {
 
@@ -44,8 +43,8 @@ function getHotel() {
             checkin_date: '2024-05-19',
             checkout_date: '2024-05-20',
             filter_by_currency: 'GBP',
-            longitude: -0.1277653,
-            latitude: 51.5074456,
+            longitude: lon,
+            latitude: lat,
             adults_number: 2,
             order_by: 'popularity',
             units: 'metric',
@@ -68,17 +67,22 @@ function getHotel() {
         const options = {
 	        method: 'GET',
 	        headers: {
-		        'X-RapidAPI-Key': 'b5a9039af8mshf35a3ef3045004ep1efb1cjsndf46ef774554',
+		        'X-RapidAPI-Key': '',
 		        'X-RapidAPI-Host': 'booking-com.p.rapidapi.com'
 	        }
         };
 
+        // fetch api to retrieve a data
         fetch(url, options)
             .then(function(response) {
                 return response.json();
             })
             .then(function(data) {
-                console.log(data);
+
+                // pass to processHotels data.result as that's where all data is we need
+                let processedHotels = processHotels(data.result)
+                console.log(processedHotels)
+
             })
             .catch(function(error) {
                 console.error("Error message: " + error);
@@ -86,4 +90,28 @@ function getHotel() {
     })
 }
 
-getHotel();
+function getHotelDetails(hotelId) {
+
+}
+// Function to extract specific data for each hotel from the api response
+function processHotels(hotelsData) {
+    return hotelsData.map(function(hotel) {
+        return {
+            hotelId: hotel.hotel_id,
+            hotelLat: hotel.latitude,
+            hotelLon: hotel.longitude,
+            hotelName: hotel.hotel_name,
+            reviewScore: hotel.review_score,
+            reviewScoreWord: hotel.review_score_word,
+            hotelPhoto: hotel.max_photo_url,
+            hotelAddressRoad: hotel.address,
+            hotelAddressPostal: hotel.zip,
+            hotelCheckIn: hotel.checkin.from,
+            hotelCheckOut: hotel.checkout.until,
+            hotelNightPrice: hotel.min_total_price,
+            bookingComLink: hotel.url,
+        }
+    })
+}
+
+// getHotel();
